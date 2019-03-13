9512e9efee6d1adc57c5c558b551d0ca9c379e79
Martin Hierholzer <martin.hierholzer@desy.de>
2019-03-13 17:34:57 +0100

    some code cleaning

--- include/FormalisedLog.h
+++ include/FormalisedLog.h
@@ -26,18 +26,9 @@ class FormalisedLog : public WContainerWidget {
   void handleInternalPath(const std::string& internalPath);
 
  private:
-  //WStackedWidget* mainStack_{nullptr};
   WStackedWidget* contentStack_{nullptr};
 
-  Wt::Auth::AuthWidget* authWidgetPtr;
-
   Session session_;
-
-  void onAuthEvent();
-  void createMenu();
-
-  void clockIn();
-  void clockOut();
 };
 
 #endif // INCLUDE_DEPLOY_TOOL_H_
